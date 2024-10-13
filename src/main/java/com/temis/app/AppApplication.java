6d5e341f70080fba49cc30ec43f3bf1d35ab481f
Gabriel Aguirre <gabominecraftero@gmail.com>
2024-10-13 13:59:55 -0600

    Faltó el scan por ConfigurationProperties

--- src/main/java/com/temis/app/AppApplication.java
+++ src/main/java/com/temis/app/AppApplication.java
@@ -2,10 +2,12 @@ package com.temis.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class AppApplication {
 
 	public static void main(String[] args) {
