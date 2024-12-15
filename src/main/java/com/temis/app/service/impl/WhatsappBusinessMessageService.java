cc1815188b8fc603388064cc9a285814f066f737
Ivan Cantu <ivancantu@MBP-NPQ00032.local>
2024-12-15 10:43:11 -0600

    Se añade bean de service.

--- src/main/java/com/temis/app/service/impl/WhatsappBusinessMessageService.java
+++ src/main/java/com/temis/app/service/impl/WhatsappBusinessMessageService.java
@@ -1,8 +1,11 @@
 package com.temis.app.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.temis.app.entity.MessageResponseEntity;
 import com.temis.app.service.MessagePlatformService;
 
+@Service
 public class WhatsappBusinessMessageService implements MessagePlatformService {
 
     @Override
