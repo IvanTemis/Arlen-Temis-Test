6615fbdc5951c90e84ed34766ae5ba82148d4dee
Gabriel-NetPay <gabominecraftero@gmail.com>
2025-01-21 20:27:27 -0600

    No Args Constructor

--- src/main/java/com/temis/app/dto/ProcessMessagesRequest.java
+++ src/main/java/com/temis/app/dto/ProcessMessagesRequest.java
@@ -4,9 +4,11 @@ import com.temis.app.entity.DocumentEntity;
 import com.temis.app.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProcessMessagesRequest {
     private String phoneNumber;
 }
